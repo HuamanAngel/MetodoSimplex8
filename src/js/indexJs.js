c4c10f50deb745665d23f9e5e5c9ba7407de802a
HuamanAngel <oxipusioxx12@gmail.com>
2021-08-25 03:07:27 -0500

    Primer prototipo

---
+++ src/js/indexJs.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#myCarousel").carousel({interval: 500});
+        
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+    
+  $(".carousel-control-prev").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
